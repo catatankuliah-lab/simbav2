4ebaa1aa93ce8decad83c4d49adfd24edd47e056
_Panjialfdn <m.panjialfianudin@gmail.com>
2024-02-08 22:52:08 +0700

    KANTOR CABANG LAPORAN

--- app/Controllers/KantorCabang/HomeController.php
+++ app/Controllers/KantorCabang/HomeController.php
@@ -13,11 +13,13 @@ class HomeController extends Controller
 
     protected $muatJanuari;
     protected $woJanuari;
+    protected $loJanuari;
 
     public function __construct()
     {
         $this->muatJanuari = new LOJanuariModel();
         $this->woJanuari = new WOJanuariModel();
+        $this->loJanuari = new WOJanuariModel();
     }
 
     public function index()
@@ -71,7 +73,7 @@ class HomeController extends Controller
     }
     public function detail_wo($nomorwo)
     {
-        $datawo = $this->woJanuari->showDetailWO($nomorwo);
+        $datawo = $this->loJanuari->showDetailWO($nomorwo);
         $data = [
             'menu1' => '',
             'menu2' => 'selected',
@@ -84,7 +86,7 @@ class HomeController extends Controller
 
     public function generateLaporanwo($nomorwo)
     {
-        $data = $this->woJanuari->woPDF($nomorwo);
+        $data = $this->loJanuari->woPDF($nomorwo);
 
         $pdf = new TCPDF('P', 'mm', 'A4', true, 'UTF-8', false);
         $pdf->SetCreator('PT Delapan Delapan Logistics');
@@ -93,16 +95,48 @@ class HomeController extends Controller
         $pdf->setPrintHeader(false);
         $pdf->setPrintFooter(false);
         $pathhasil = $data[0]->path_wo;
+
+        // Halaman untuk dokument working order
         $pdf->AddPage('P', 'A4');
         $leftImagePath = FCPATH . DIRECTORY_SEPARATOR . $pathhasil . DIRECTORY_SEPARATOR . $data[0]->file_wo;
         $pdf->Image($leftImagePath, 10, 10, $pdf->GetPageWidth(), $pdf->getPageHeight());
+
+        // Halaman untuk dokumet penyaluran
+        $pdf->AddPage('L', 'A4');
+        $pdf->Cell(0, 10, 'REKAPITULASI HARIAN BANTUAN PANGAN CADANGAN BERAS PEMERINTAH', +$data[0]->tanggal_muat, 0, false, 'C', 0, '', 0, false, 'M', 'M');
+        $pdf->Ln(10); // Spasi antara tabel dan entri baru
+        $pdf->Cell(0, 10, 'TRANSPORTER : PT LOGISTICS DELAPAN DELAPAN', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+        $pdf->Cell(0, 10, 'GUDANG MUAT : ', + $data[0]->nama_gudang, 0, false, 'C', 0, '', 0, false, 'M', 'M');
+
+
+
+        // Looping data Loading Order (LO)
         foreach ($data as $row) {
+            // LO 
             $pdf->AddPage('P', 'A4');
-            $leftImagePath = FCPATH . DIRECTORY_SEPARATOR . $row->path_wo . DIRECTORY_SEPARATOR . $row->file_dokumen_muat;
+            $leftImagePath = FCPATH . DIRECTORY_SEPARATOR . $row->path_wo . DIRECTORY_SEPARATOR . $row->file_uplaod_lo;
             $pdf->Image($leftImagePath, 10, 10, $pdf->GetPageWidth(), $pdf->getPageHeight());
 
+            // lopiing surat jalan by nomor lo
+            foreach ($data as $row) {
+                $pdf->AddPage('P', 'A4');
+                $leftImagePath = FCPATH . DIRECTORY_SEPARATOR . $row->path_wo . DIRECTORY_SEPARATOR . $row->file_surat_jalan;
+                $pdf->Image($leftImagePath, 10, 10, $pdf->GetPageWidth(), $pdf->getPageHeight());
+            }
+
+            // Dokumen DO BULOG
             $pdf->AddPage('P', 'A4');
-            $leftImagePath = FCPATH . DIRECTORY_SEPARATOR . $row->path_wo . DIRECTORY_SEPARATOR . $row->file_surat_jalan;
+            $leftImagePath = FCPATH . DIRECTORY_SEPARATOR . $row->path_wo . DIRECTORY_SEPARATOR . $row->file_upload_do;
+            $pdf->Image($leftImagePath, 10, 10, $pdf->GetPageWidth(), $pdf->getPageHeight());
+
+            // Dokumen Surat Jalan BULOG
+            $pdf->AddPage('P', 'A4');
+            $leftImagePath = FCPATH . DIRECTORY_SEPARATOR . $row->path_wo . DIRECTORY_SEPARATOR . $row->file_upload_sj_bulog;
+            $pdf->Image($leftImagePath, 10, 10, $pdf->GetPageWidth(), $pdf->getPageHeight());
+
+            // Dokumen BAST BULOG
+            $pdf->AddPage('P', 'A4');
+            $leftImagePath = FCPATH . DIRECTORY_SEPARATOR . $row->path_wo . DIRECTORY_SEPARATOR . $row->file_upload_bast_bulog;
             $pdf->Image($leftImagePath, 10, 10, $pdf->GetPageWidth(), $pdf->getPageHeight());
         }
         if (!file_exists($pathhasil)) {
